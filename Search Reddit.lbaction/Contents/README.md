abdc3d0557e14ad4b38bc3713ac1f34fd71d030b
Edward Cheng <ewlcheng@gmail.com>
2022-02-27 14:08:51 -0700

    First launchbar extensions commit

---
+++ Search Reddit.lbaction/Contents/README.md
@@ -0,0 +1,5 @@
+# launchbar-reddit-search
+
+Launchbar Action for searching Reddit.
+
+![example](https://user-images.githubusercontent.com/25830486/155899657-9442a0b8-52b6-40be-8255-9ea6364cabd1.gif)
---
+++ Search Reddit.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,129 @@
+// LaunchBar Action Script
+
+const redditUrl = "https://www.reddit.com";
+
+function run(argument) {
+    if (argument == undefined) {
+        // Inform the user that there was no argument
+        LaunchBar.alert("No argument was passed to the action");
+    } else {
+        let query = null;
+        let sort = "";
+        if (typeof argument === "object") {
+            query = parseQuery(argument.query);
+            sort = argument.sort;
+        } else {
+            query = parseQuery(argument);
+        }
+
+        const result = HTTP.getJSON(createSearchUrl(query.subreddit, true, query.query, "", 10, sort));
+        const searchUrl = createSearchUrl(query.subreddit, false, query.query, "", 0, sort);
+
+        if (result.data != undefined) {
+            if (result.data.data.children) {
+                const launchBarResults = [];
+                for (let i = 0; i < result.data.data.children.length; i++) {
+                    const data = result.data.data.children[i];
+                    launchBarResults.push({
+                        title: data.data.title,
+                        label: "r/" + data.data.subreddit,
+                        subtitle: "Posted " + new Date(data.data.created_utc * 1000).toLocaleString(),
+                        url: joinUrl(redditUrl, data.data.permalink),
+                    });
+                }
+
+                if (launchBarResults.length) {
+                    launchBarResults.push({
+                        title: "Sort by...",
+                        children: [
+                            {
+                                title: "Relevance",
+                                action: "run",
+                                actionArgument: { query: argument, sort: "relevance" },
+                            },
+                            { title: "Hot", action: "run", actionArgument: { query: argument, sort: "hot" } },
+                            { title: "Top", action: "run", actionArgument: { query: argument, sort: "top" } },
+                            { title: "Latest", action: "run", actionArgument: { query: argument, sort: "new" } },
+                            { title: "Comments", action: "run", actionArgument: { query: argument, sort: "comments" } },
+                        ],
+                    });
+                }
+
+                launchBarResults.push({ title: "Show all results on Reddit", url: searchUrl });
+
+                return launchBarResults;
+            }
+        } else if (result.error != undefined) {
+            LaunchBar.alert("Unable to search Reddit", result.error);
+        }
+    }
+}
+
+function parseQuery(query) {
+    var splitQuery = query.split(" ");
+    let newQuery = "";
+    let subreddit = "";
+    for (let i = 0; i < splitQuery.length; i++) {
+        const element = splitQuery[i];
+        if (element.startsWith("r/")) {
+            subreddit = element;
+        } else {
+            newQuery += element + " ";
+        }
+    }
+
+    newQuery = newQuery.trim();
+
+    return {
+        subreddit: subreddit,
+        query: newQuery,
+    };
+}
+
+function createSearchUrl(subreddit = "", json = false, query = "", type = "", limit = 0, sort = "") {
+    let url = redditUrl;
+
+    if (subreddit) {
+        url = joinUrl(url, subreddit);
+    }
+
+    url = joinUrl(url, json ? "search.json" : "search");
+
+    if (subreddit || query || type || limit || sort) {
+        const params = [];
+
+        if (subreddit) {
+            params.push({ key: "restrict_sr", value: "true" });
+        }
+
+        if (query) {
+            params.push({ key: "q", value: query });
+        }
+
+        if (type) {
+            params.push({ key: "type", value: type });
+        }
+
+        if (limit) {
+            params.push({ key: "limit", value: limit.toString() });
+        }
+
+        if (sort) {
+            params.push({ key: "sort", value: sort });
+        }
+
+        const queryParameters = params.reduce(function (previous = "", current) {
+            return previous + "&" + current.key + "=" + encodeURIComponent(current.value);
+        }, "");
+
+        url = url + "?" + queryParameters;
+    }
+
+    return url;
+}
+
+function joinUrl(part1, part2) {
+    const p1 = part1.endsWith("/") ? part1.substring(0, part1.length - 1) : part1;
+    const p2 = part2.startsWith("/") ? part2.substring(1) : part2;
+    return p1 + "/" + p2;
+}
